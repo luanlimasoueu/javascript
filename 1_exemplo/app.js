290ca75098f29ad354f2944eb118196ca7657fb9
okluan <luanemerson8@gmail.com>
2024-08-21 16:54:38 -0300

    Alterações

---
+++ 1_exemplo/app.js
@@ -0,0 +1,20 @@
+import sqlite3 from 'sqlite3'
+import {open} from 'sqlite'
+
+async function criarEPopularTabelaUsuarios ( nome, sobrenome){
+
+    const db = await open({
+        filename: './banco.db',
+        driver: sqlite3.Database
+    });
+    db.run(
+        "CREATE TABLE IF NOT EXISTS usuarios ( iD INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT)"
+    )
+
+    db.run(" INSERT INTO usuarios (nome, sobrenome) VALUES (?, ?)",
+        [nome,
+        sobrenome
+        ]);
+}
+
+criarEPopularTabelaUsuarios("Alon", "Pinheiro")
---
+++ 2_exemplo/2_4_exemplo.js
@@ -0,0 +1,11 @@
+let age = 25;
+let result = age >= 18 ? "You are an adult" : "You are not an adult";
+
+console.log(result);
+
+
+let num1 = 5;
+let num2 = "5";
+
+console.log(num1 !== num2); // true
+
