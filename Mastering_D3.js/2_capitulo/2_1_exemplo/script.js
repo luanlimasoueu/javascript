ef7a90b1f07c96f60c50499bec63b7d5c86ba15e
okluan <luanemerson8@gmail.com>
2025-04-04 00:47:31 -0300

    Study

--- Mastering_D3.js/2_capitulo/2_1_exemplo/script.js
+++ Mastering_D3.js/2_capitulo/2_1_exemplo/script.js
@@ -30,3 +30,47 @@ selection.each(function(d) {
 d3.select(this).append('p').html(d);
 });
 });
+
+
+function initDiv(selection) {
+	selection.each(function(data) {
+	d3.select(this).append('p')
+	.html(data);
+	});
+	}
+
+
+d3.select('#chart2').selectAll('div.data-item')
+	.data(data)
+	.enter()
+	.append('div')
+	.attr('class', 'data-item')
+	.call(initDiv);
+
+
+var width = 400, height = 40;
+
+function chart(selection) {
+	selection.each(function(data) {
+	// Select and bind the svg element.
+	var div = d3.select(this).attr('class', 'data-item'),
+	svg = div.selectAll('svg').data([data]),
+	svgEnter = svg.enter();
+	// Append the svg and the rectangle on enter.
+	svgEnter.append('svg')
+	.attr('width', width)
+	.attr('height', height)
+	.append('rect')
+	.attr('width', width)
+	.attr('height', height)
+	.attr('fill', 'black');
+	});
+	}
+
+
+d3.select('#chart3').selectAll('div.data-item')
+.data(data)
+.enter()
+.append('div')
+.attr('class', 'data-item')
+.call(chart);
