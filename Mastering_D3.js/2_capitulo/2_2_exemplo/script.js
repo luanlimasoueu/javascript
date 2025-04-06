12b1375d0806f60559dd7bf095fbeb817cffa12f
okluan <luanemerson8@gmail.com>
2025-04-06 00:09:44 -0300

    Study

--- Mastering_D3.js/2_capitulo/2_2_exemplo/script.js
+++ Mastering_D3.js/2_capitulo/2_2_exemplo/script.js
@@ -1,23 +1,40 @@
-var barcodeChart = function() {
-	// Chart Variables.Attributes
-	var width = 600,
-	height = 30,
-	margin = {top: 5, right: 5, bottom: 5, left: 5};
-	function chart(selection) {
-	// Chart creation…
-	}
-	// Accessor function for the width
-	chart.width = function(value) {
-	if (!arguments.length) { return width; }
-	width = value;
-	// Returns the chart to allow method chaining.
-	return chart;
-	};
-	// Accessor functions for the height and the margin…
-	return chart;
-};
+var data = [
+	{name: 'AAPL', mentions: ['...'], byHour: 34.3},
+	{name: 'MSFT', mentions: ['...'], byHour: 11.1},
+	{name: 'GOOG', mentions: ['...'], byHour: 19.2},
+	{name: 'NFLX', mentions: ['...'], byHour: 6.7}
+	];
 
-// Create and configure the chart.
-var barcode = barcodeChart()
-.width(600)
-.height(25);
+// Create a table element.
+var table = d3.select('#chart').selectAll('table')
+	.data([data])
+	.enter()
+	.append('table')
+	.attr('class', 'table table-condensed');
+
+// Append the table head and body to the table.
+var tableHead = table.append('thead'),
+tableBody = table.append('tbody');
+
+tableHead.append('tr').selectAll('th')
+.data(['Name', 'Today Mentions', 'mentions/hour'])
+.enter()
+.append('th')
+.text(function(d) { return d; });
+
+
+var rows = tableBody.selectAll('tr')
+.data(data)
+.enter()
+.append('tr');
+
+rows.append('td')
+.text(function(d) { return d.name; });
+
+rows.append('td')
+.datum(function(d) { return d.mentions; })
+
+rows.append('td').append('p')
+.attr('class', 'pull-right')
+.html(function(d) { return d.byHour; });
+
